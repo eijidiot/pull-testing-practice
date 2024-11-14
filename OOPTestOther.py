11b8c94b175ab0c23d77c2c66fce1c3af9127a4d
Glenn Ace Limwell Eiji <92159514+eijidiot@users.noreply.github.com>
2024-11-14 13:38:34 +0800

    Create OOPTestOther.py
    test

---
+++ OOPTestOther.py
@@ -0,0 +1,18 @@
+#OOP Testing
+
+class <ClassName>:
+
+    <class_attribute_name> = <value>
+
+    def __init__(self,<param1>, <param2>, ...):
+        self.<attr1> = <param1>
+        self.<attr2> = <param2>
+        .
+        .
+        .
+        # As many attributes as needed
+
+   def <method_name>(self, <param1>, ...):
+       <code>
+
+   # As many methods as needed
